bf8f2c0d863923a31d2aacfce7c733d10431121f
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-10-01 04:44:27 +0400

    style: standardize style

--- script.ino
+++ script.ino
@@ -6,48 +6,48 @@ const int RECV_PIN = A5;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-#define enA 10 //Enable1 L298 Pin enA 
-#define in1 9 //Right Motor forward L298 Pin in1 
-#define in2 8 //Right Motor backward L298 Pin in1 
-#define in3 7 //Left Motor forward L298 Pin in1 
-#define in4 6 //Left Motor backward L298 Pin in1 
-#define enB 5 //Enable2 L298 Pin enB 
+#define enA 10 // Enable1 L298 Pin enA 
+#define in1 9 // Right Motor forward L298 Pin in1 
+#define in2 8 // Right Motor backward L298 Pin in1 
+#define in3 7 // Left Motor forward L298 Pin in1 
+#define in4 6 // Left Motor backward L298 Pin in1 
+#define enB 5 // Enable2 L298 Pin enB 
 
 #define servo A4
 
-#define R_S A0 //ir sensor Right
-#define L_S A1 //ir sensor Left
+#define R_S A0 // IR sensor Right
+#define L_S A1 // IR sensor Left
 
-#define echo A2    //Echo pin
-#define trigger A3 //Trigger pin
+#define echo A2    // Echo pin
+#define trigger A3 // Trigger pin
 
 int distance_L, distance_F = 30, distance_R;
 long distance;
-int set = 20; //set the distance to be 20cm
+int set = 20; // Set the distance to be 20cm
 
-int bt_ir_data; // variable to receive data from the serial port and IRremote
-int Speed = 130; // speed of the motor
-int mode = 0; // mode of working type
+int bt_ir_data; // Variable to receive data from the serial port and IRremote
+int Speed = 130; // Speed of the motor
+int mode = 0; // Mode of working type
 
-void setup() { // put your setup code here, to run once
+void setup() { // Put your setup code here, to run once
 
-  pinMode(R_S, INPUT); // declare if sensor as input
-  pinMode(L_S, INPUT); // declare ir sensor as input
+  pinMode(R_S, INPUT); // Declare if sensor as input
+  pinMode(L_S, INPUT); // Declare ir sensor as input
 
-  pinMode(echo, INPUT );// declare ultrasonic sensor Echo pin as input
+  pinMode(echo, INPUT ); // declare ultrasonic sensor Echo pin as input
   pinMode(trigger, OUTPUT); // declare ultrasonic sensor Trigger pin as Output
 
-  pinMode(enA, OUTPUT); // declare as output for L298 Pin enA
-  pinMode(in1, OUTPUT); // declare as output for L298 Pin in1
-  pinMode(in2, OUTPUT); // declare as output for L298 Pin in2
-  pinMode(in3, OUTPUT); // declare as output for L298 Pin in3
-  pinMode(in4, OUTPUT); // declare as output for L298 Pin in4
-  pinMode(enB, OUTPUT); // declare as output for L298 Pin enB
+  pinMode(enA, OUTPUT); // Declare as output for L298 Pin enA
+  pinMode(in1, OUTPUT); // Declare as output for L298 Pin in1
+  pinMode(in2, OUTPUT); // Declare as output for L298 Pin in2
+  pinMode(in3, OUTPUT); // Declare as output for L298 Pin in3
+  pinMode(in4, OUTPUT); // Declare as output for L298 Pin in4
+  pinMode(enB, OUTPUT); // Declare as output for L298 Pin enB
 
   irrecv.enableIRIn(); // Start the receiver
   irrecv.blink13(true);
 
-  Serial.begin(9600); // start serial communication at 9600bps
+  Serial.begin(9600); // Start serial communication at 9600bps
   BT_Serial.begin(9600);
 
   pinMode(servo, OUTPUT);
@@ -68,7 +68,7 @@ void setup() { // put your setup code here, to run once
 
 void loop() {
 
-  if (BT_Serial.available() > 0) { //if some date is sent, reads it and saves in state
+  if (BT_Serial.available() > 0) { // If some date is sent, reads it and saves in state
     bt_ir_data = BT_Serial.read();
     Serial.println(bt_ir_data);
     if (bt_ir_data > 20) {
@@ -78,22 +78,22 @@ void loop() {
 
   if (irrecv.decode()) {
     Serial.println(results.value, HEX);
-    bt_ir_data = IRremote_data();
+    bt_ir_data = irRemoteData();
     Serial.println(bt_ir_data);
     irrecv.resume(); // Receive the next value
     delay(100);
   }
 
   if (bt_ir_data == 8) {
-    mode = 0;  //Manual Android Application and IR Remote Control Command
+    mode = 0;  // Manual Android Application and IR Remote Control Command
     Stop();
   }
   else if (bt_ir_data == 9) {
-    mode = 1;  //Auto Line Follower Command
+    mode = 1;  // Auto Line Follower Command
     Speed = 130;
   }
   else if (bt_ir_data == 10) {
-    mode = 2;  //Auto Obstacle Avoiding Command
+    mode = 2;  // Auto Obstacle Avoiding Command
     Speed = 255;
   }
 
@@ -101,99 +101,87 @@ void loop() {
   analogWrite(enB, Speed); // Write The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed
 
   if (mode == 0) {
-    robotic_car_control();
+    roboticCarControl();
   }
   else if (mode == 1) {
-    line_follower_control();
+    lineFollowerControl();
   }
   else if (mode == 2) {
-    obstacle_avoiding_control();
+    obstacleAvoidingControl();
   }
 
   delay(10);
 }
 
-void robotic_car_control() {
-  key_control_command();
-  voice_control_command();
+void roboticCarControl() {
+  keyControlCommand();
+  voiceControlCommand();
 }
 
-void key_control_command() {
-  //===============================================================================
+void keyControlCommand() {
+  // ===============================================================================
   //                          Key Control Command
-  //===============================================================================
-  if (bt_ir_data == 1)
-  {
-    forward(); // if the bt_data is '1' the DC motor will go forward
+  // ===============================================================================
+  if (bt_ir_data == 1) {
+    forward(); // If the bt_data is '1' the DC motor will go forward
   }
-  else if (bt_ir_data == 2)
-  {
-    backward(); // if the bt_data is '2' the motor will Reverse
+  else if (bt_ir_data == 2) {
+    backward(); // If the bt_data is '2' the motor will reverse
   }
-  else if (bt_ir_data == 3)
-  {
-    turnLeft(); // if the bt_data is '3' the motor will turn left
+  else if (bt_ir_data == 3) {
+    turnLeft(); // If the bt_data is '3' the motor will turn left
   }
-  else if (bt_ir_data == 4)
-  {
-    turnRight(); // if the bt_data is '4' the motor will turn right
+  else if (bt_ir_data == 4) {
+    turnRight(); // If the bt_data is '4' the motor will turn right
   }
-  else if (bt_ir_data == 5)
-  {
-    Stop(); // if the bt_data '5' the motor will Stop
+  else if (bt_ir_data == 5) {
+    Stop(); // If the bt_data '5' the motor will Stop
   }
 }
 
-void voice_control_command() {
-  //===============================================================================
+void voiceControlCommand() {
+  // ===============================================================================
   //                          Voice Control Command
-  //===============================================================================
-  else if (bt_ir_data == 6)
-  {
+  // ===============================================================================
+  if (bt_ir_data == 6) {
     turnLeft();
     delay(400);
     bt_ir_data = 5;
   }
-  else if (bt_ir_data == 7)
-  {
+  else if (bt_ir_data == 7) {
     turnRight();
     delay(400);
     bt_ir_data = 5;
   }
 }
 
-void line_follower_control() {
-  //===============================================================================
+void lineFollowerControl() {
+  // ===============================================================================
   //                          Line Follower Control
-  //===============================================================================
+  // ===============================================================================
 
-  if (digitalRead(L_S) == HIGH && digitalRead(R_S) == LOW)
-  {
+  if (digitalRead(L_S) == HIGH && digitalRead(R_S) == LOW) {
     turnRight();
   }
-  else if (digitalRead(R_S) == HIGH && digitalRead(L_S) == LOW)
-  {
+  else if (digitalRead(R_S) == HIGH && digitalRead(L_S) == LOW) {
     turnLeft();
   }
   else
     forward();
 }
 
-void obstacle_avoiding_control() {
-  //===============================================================================
+void obstacleAvoidingControl() {
+  // ===============================================================================
   //                          Obstacle Avoiding Control
-  //===============================================================================
-  distance_F = Ultrasonic_read();
+  // ===============================================================================
+  distance_F = ultrasonicRead();
   Serial.print("S=");
   Serial.println(distance_F);
-  if (distance_F > set)
-  {
+  if (distance_F > set) {
     forward();
   }
   else
-  {
-    Check_side();
-  }
+    checkSide();
 }
 
 void backward() {
@@ -203,8 +191,9 @@ void backward() {
   digitalWrite(in4, HIGH); // Left Motor Backward
 }
 
-long IRremote_data() {
+long irRemoteData() {
   int IR_data;
+
   if (results.value == 0xFF02FD) {
     IR_data = 1;
   }
@@ -229,6 +218,7 @@ long IRremote_data() {
   else if (results.value == 0xFF7A85) {
     IR_data = 10;
   }
+
   return IR_data;
 }
 
@@ -241,8 +231,8 @@ void servoPulse (int pin, int angle) {
 }
 
 
-//**********************Ultrasonic_read****************************
-long Ultrasonic_read() {
+//**********************ultrasonicRead****************************
+long ultrasonicRead() {
   digitalWrite(trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(trigger, HIGH);
@@ -268,20 +258,20 @@ void compareDistance() {
   }
 }
 
-void Check_side() {
+void checkSide() {
   Stop();
   delay(100);
   for (int angle = 70; angle <= 140; angle += 5)  {
     servoPulse(servo, angle);
   }
   delay(300);
-  distance_L = Ultrasonic_read();
+  distance_L = ultrasonicRead();
   delay(100);
   for (int angle = 140; angle >= 0; angle -= 5)  {
     servoPulse(servo, angle);
   }
   delay(500);
-  distance_R = Ultrasonic_read();
+  distance_R = ultrasonicRead();
   delay(100);
   for (int angle = 0; angle <= 70; angle += 5)  {
     servoPulse(servo, angle);
@@ -290,30 +280,30 @@ void Check_side() {
   compareDistance();
 }
 
-void forward() { //forward
-  digitalWrite(in1, HIGH); //Right Motor forward Pin
-  digitalWrite(in2, LOW);  //Right Motor backward Pin
-  digitalWrite(in3, HIGH);  //Left Motor forward Pin
-  digitalWrite(in4, LOW); //Left Motor backward Pin
+void forward() {
+  digitalWrite(in1, HIGH); // Right Motor forward Pin
+  digitalWrite(in2, LOW);  // Right Motor backward Pin
+  digitalWrite(in3, HIGH);  // Left Motor forward Pin
+  digitalWrite(in4, LOW); // Left Motor backward Pin
 }
 
-void turnRight() { //turnRight
-  digitalWrite(in1, LOW);  //Right Motor forward Pin
-  digitalWrite(in2, HIGH); //Right Motor backward Pin
-  digitalWrite(in3, HIGH);  //Left Motor forward Pin
-  digitalWrite(in4, LOW); //Left Motor backward Pin
+void turnRight() {
+  digitalWrite(in1, LOW);  // Right Motor forward Pin
+  digitalWrite(in2, HIGH); // Right Motor backward Pin
+  digitalWrite(in3, HIGH);  // Left Motor forward Pin
+  digitalWrite(in4, LOW); // Left Motor backward Pin
 }
 
-void turnLeft() { //turnLeft
-  digitalWrite(in1, HIGH); //Right Motor forward Pin
-  digitalWrite(in2, LOW);  //Right Motor backward Pin
-  digitalWrite(in3, LOW); //Left Motor forward Pin
-  digitalWrite(in4, HIGH);  //Left Motor backward Pin
+void turnLeft() {
+  digitalWrite(in1, HIGH); // Right Motor forward Pin
+  digitalWrite(in2, LOW);  // Right Motor backward Pin
+  digitalWrite(in3, LOW); // Left Motor forward Pin
+  digitalWrite(in4, HIGH);  // Left Motor backward Pin
 }
 
-void Stop() { //stop
-  digitalWrite(in1, LOW); //Right Motor forward Pin
-  digitalWrite(in2, LOW); //Right Motor backward Pin
-  digitalWrite(in3, LOW); //Left Motor backward Pin
-  digitalWrite(in4, LOW); //Left Motor forward Pin
+void Stop() {
+  digitalWrite(in1, LOW); // Right Motor forward Pin
+  digitalWrite(in2, LOW); // Right Motor backward Pin
+  digitalWrite(in3, LOW); // Left Motor backward Pin
+  digitalWrite(in4, LOW); // Left Motor forward Pin
 }
